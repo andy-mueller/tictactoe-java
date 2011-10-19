8e70765eac9b55cb705efdf24d102dbbbe7853e2
unknown <Andy@Direwolf.(none)>
2011-10-19 20:50:02 +0200

    cursor input triggers repaint

--- tictacttoe-client-jcurses/src/test/java/com/crudetech/tictactoe/client/jcurses/GridWidgetTest.java
+++ tictacttoe-client-jcurses/src/test/java/com/crudetech/tictactoe/client/jcurses/GridWidgetTest.java
@@ -1,10 +1,8 @@
 package com.crudetech.tictactoe.client.jcurses;
 
 import com.crudetech.tictactoe.game.Grid;
-import jcurses.system.InputChar;
 import org.junit.Test;
 
-import static jcurses.system.InputChar.*;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.spy;
@@ -28,65 +26,13 @@ public class GridWidgetTest {
         verify(cursor).setLocation(firstCell);
     }
 
-    @Test
-    public void arrowDownMovesCursorOneDown() {
-        GridWidget.Cursor cursor = spy(new GridWidget.Cursor());
-        GridWidget w = new StandAloneGridWidget(cursor);
 
-        boolean handled = w.handleInput(new InputChar(KEY_DOWN));
-        assertThat(handled, is(true));
 
-        verify(cursor).moveDown();
-    }
 
-    @Test
-    public void arrowUpMovesCursorOneUp() {
-        GridWidget.Cursor cursor = spy(new GridWidget.Cursor());
-        GridWidget w = new StandAloneGridWidget(cursor);
-
-        boolean handled = w.handleInput(new InputChar(KEY_UP));
-        assertThat(handled, is(true));
-
-        verify(cursor).moveUp();
-    }
-
-    @Test
-    public void arrowLeftMovesCursorOneLeft() {
-        GridWidget.Cursor cursor = spy(new GridWidget.Cursor());
-        GridWidget w = new StandAloneGridWidget(cursor);
-
-        boolean handled = w.handleInput(new InputChar(KEY_LEFT));
-        assertThat(handled, is(true));
-
-        verify(cursor).moveLeft();
-    }
-
-    @Test
-    public void arrowRightMovesCursorOneRight() {
-        GridWidget.Cursor cursor = spy(new GridWidget.Cursor());
-        GridWidget w = new StandAloneGridWidget(cursor);
-
-        boolean handled = w.handleInput(new InputChar(KEY_RIGHT));
-        assertThat(handled, is(true));
-
-        verify(cursor).moveRight();
-    }
-    @Test
-    public void anyCharInputRaisesRepaint() {
-        GridWidget.Cursor cursor = spy(new GridWidget.Cursor());
-
-        StandAloneGridWidget w = new StandAloneGridWidget(cursor);
-
-        w.handleInput(new InputChar(KEY_RIGHT));
-
-        assertThat(w.getRepaints(), is(1));
-    }
-
-    //handleInput raises repaint
-    //repaint cals setOn
 
     // handle inpu makes cross
     //handleInput set nought
+    ////handle fails on there chars
     //set mark failswhenallready marked
 
 
