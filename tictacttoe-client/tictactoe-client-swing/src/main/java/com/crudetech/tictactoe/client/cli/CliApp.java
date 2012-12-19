6b2f7888b05d799a02cad7c64a0cbbe51cd50b7a
Andreas Mueller <andreas.b.mueller@freenet.de>
2012-12-19 09:13:04 +0100

    -refactoring

--- tictacttoe-client/tictactoe-client-swing/src/main/java/com/crudetech/tictactoe/client/cli/CliApp.java
+++ tictacttoe-client/tictactoe-client-swing/src/main/java/com/crudetech/tictactoe/client/cli/CliApp.java
@@ -1,12 +1,9 @@
 package com.crudetech.tictactoe.client.cli;
 
 
-import com.crudetech.event.Event;
-import com.crudetech.event.EventSupport;
 import com.crudetech.tictactoe.game.AlphaBetaPruningPlayer;
 import com.crudetech.tictactoe.game.ComputerPlayer;
 import com.crudetech.tictactoe.game.Grid;
-import com.crudetech.tictactoe.ui.CellEventObject;
 import com.crudetech.tictactoe.ui.HumanPlayer;
 import com.crudetech.tictactoe.ui.HumanVsComputerPlayerInteractor;
 
@@ -26,7 +23,7 @@ public class CliApp {
 
         TextGridWidgetUiView textGridWidgetUiView = new TextGridWidgetUiView(widget, out);
         TextUiFeedbackChannel textUiFeedbackChannel = new TextUiFeedbackChannel(out);
-        final HumanPlayer humanPlayer = new CliHumanPlayer(textGridWidgetUiView, textUiFeedbackChannel, out, input, widget);
+        final HumanPlayer humanPlayer = new TextHumanPlayer(textGridWidgetUiView, textUiFeedbackChannel, out, input, widget);
         HumanVsComputerPlayerInteractor interactor =
                 HumanVsComputerPlayerInteractor.builder()
                         .setComputerPlayer(aiPlayer)
@@ -36,30 +33,4 @@ public class CliApp {
         interactor.startWithHumanPlayer(Grid.Mark.Cross);
     }
 
-    private static class CliHumanPlayer extends HumanPlayer {
-        final EventSupport<CellEventObject<TextGridWidget>> cellEnterEvent = new EventSupport<CellEventObject<TextGridWidget>>();
-        private final PrintWriter out;
-        private final TextUserInput input;
-        private final TextGridWidget widget;
-
-        public CliHumanPlayer(TextGridWidgetUiView textGridWidgetUiView, TextUiFeedbackChannel textUiFeedbackChannel, PrintWriter out, TextUserInput input, TextGridWidget widget) {
-            super(textGridWidgetUiView, textUiFeedbackChannel);
-            this.out = out;
-            this.input = input;
-            this.widget = widget;
-        }
-
-        @Override
-        public void yourTurn(Grid actualGrid) {
-            super.yourTurn(actualGrid);
-            out.println("Make your move");
-            Grid.Location location = input.nextLocation();
-            cellEnterEvent.fireEvent(new CellEventObject<TextGridWidget>(widget, location));
-        }
-
-        @Override
-        public Event<? extends CellEventObject<?>> makeMove() {
-            return cellEnterEvent;
-        }
-    }
 }
