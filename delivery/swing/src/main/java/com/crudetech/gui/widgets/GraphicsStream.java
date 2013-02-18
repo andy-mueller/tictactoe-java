adf3d5ed6232eaee5df2ca5fdf0f89398e014dbb
Andreas Mueller <andreas.b.mueller@freenet.de>
2013-02-18 10:04:04 +0100

    Introduced GraphicsStram to separate drawing engine from tic tac toe logic

---
+++ delivery/swing/src/main/java/com/crudetech/gui/widgets/GraphicsStream.java
@@ -0,0 +1,11 @@
+package com.crudetech.gui.widgets;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andy
+ * Date: 18.02.13
+ * Time: 09:30
+ * To change this template use File | Settings | File Templates.
+ */
+public interface GraphicsStream {
+}
