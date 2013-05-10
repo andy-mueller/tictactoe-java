c49b9438b6cc5b9a9e89409ba9840dcb89b2fe36
Andreas Mueller <andreas.b.mueller@freenet.de>
2013-05-10 10:15:39 +0200

    -corrected typo

--- delivery/swing/src/main/java/com/crudetech/gui/widgets/CoordinateSystem.java
+++ delivery/swing/src/main/java/com/crudetech/gui/widgets/CoordinateSystem.java
@@ -11,7 +11,7 @@ public class CoordinateSystem {
         return new CoordinateSystem();
     }
 
-    public static CoordinateSystem idenitity() {
+    public static CoordinateSystem identity() {
         return world();
     }
 
