f3920d341448f33a3ec6e4d66cd268cfeb9ad379
Andreas Mueller <andreas.b.mueller@freenet.de>
2013-06-11 09:57:49 +0200

    -factored hit test into cell widget -small renaming for consistency in the hit test call chain

--- delivery/swing/src/main/java/com/crudetech/tictactoe/delivery/gui/widgets/GridCellHit.java
+++ delivery/swing/src/main/java/com/crudetech/tictactoe/delivery/gui/widgets/GridCellHit.java
@@ -1,55 +1,32 @@
 package com.crudetech.tictactoe.delivery.gui.widgets;
 
-import com.crudetech.collections.Pair;
-import com.crudetech.functional.UnaryFunction;
+
 import com.crudetech.gui.widgets.Point;
 import com.crudetech.gui.widgets.Rectangle;
 import com.crudetech.tictactoe.game.Grid;
 
-import static com.crudetech.query.Query.from;
-import static com.crudetech.tictactoe.game.GridCells.getAtLocation;
-
 public class GridCellHit {
-    private final Pair<Rectangle, Grid.Location> hitInfo;
-    private static final Pair<Rectangle, Grid.Location> NoHit =
-            new Pair<>(new Rectangle(-1, -1, -1, -1), null);
 
-    GridCellHit(Iterable<Grid.Cell> cells, int x, int y, Rectangle[][] cellBoundaries) {
-        hitInfo = from(cells).select(isContainedIn(x, y, cellBoundaries)).where(notNoHit()).firstOr(NoHit);
+    static GridCellHit NoHit = new GridCellHit(null, false);
+    private final boolean hasHit;
+
+
+    private final Grid.Location location;
+
+    GridCellHit(Grid.Location location, Rectangle boundary, Point hitPoint) {
+        this(location, boundary.contains(hitPoint));
     }
-    GridCellHit(Iterable<Grid.Cell> cells, Point hit, Rectangle[][] cellBoundaries) {
-        this(cells, hit.x, hit.y, cellBoundaries);
+
+    GridCellHit(Grid.Location location, boolean hasHit) {
+        this.location = location;
+        this.hasHit = hasHit;
     }
 
     public Grid.Location getHit() {
-        return hitInfo.getSecond();
+        return location;
     }
 
     public boolean hasHit() {
-        return !NoHit.equals(hitInfo);
-    }
-
-
-    private UnaryFunction<Pair<Rectangle, Grid.Location>, Boolean> notNoHit() {
-        return new UnaryFunction<Pair<Rectangle, Grid.Location>, Boolean>() {
-            @Override
-            public Boolean execute(Pair<Rectangle, Grid.Location> rectangleLocationPair) {
-                return !rectangleLocationPair.equals(NoHit);
-            }
-        };
-    }
-
-    private UnaryFunction<Grid.Cell, Pair<Rectangle, Grid.Location>> isContainedIn(final int x, final int y, final Rectangle[][] gridMarkLocations) {
-        return new UnaryFunction<Grid.Cell, Pair<Rectangle, Grid.Location>>() {
-            @Override
-            public Pair<Rectangle, Grid.Location> execute(Grid.Cell cell) {
-                Grid.Location location = cell.getLocation();
-                Rectangle hitRect = getAtLocation(gridMarkLocations, location);
-                if (hitRect.contains(x, y)) {
-                    return new Pair<>(hitRect, location);
-                }
-                return NoHit;
-            }
-        };
+        return hasHit;
     }
 }
