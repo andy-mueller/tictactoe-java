f81bc209270d6faa4cd3e13deb09eecd96a15c8c
andy-mueller <andreas.b.mueller@freenet.de>
2014-03-11 09:36:50 +0100

    -extended test for multiple context levels

--- delivery/swing/src/test/java/com/crudetech/gui/widgets/GraphicsStreamProtocolTest.java
+++ delivery/swing/src/test/java/com/crudetech/gui/widgets/GraphicsStreamProtocolTest.java
@@ -7,13 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Features.class)
 public class GraphicsStreamProtocolTest {
-    static class ExampleWidget implements Widget {
-        @Override
-        public void paint(GraphicsStream pipe) {
-            try (GraphicsStream.Context ctx = pipe.newContext()) {
-                ctx.drawImage(null);
-            }
-        }
+    static abstract class AWidget implements Widget {
+        static final Image AnyImage = null;
 
         @Override
         public CoordinateSystem coordinateSystem() {
@@ -22,6 +17,27 @@ public class GraphicsStreamProtocolTest {
 
         @Override
         public void setCoordinateSystem(CoordinateSystem coordinates) {
+        }
+    }
+
+    static class ExampleWidget extends AWidget {
+        private final ExampleSubWidget subWidget = new ExampleSubWidget();
+
+        @Override
+        public void paint(GraphicsStream pipe) {
+            try (GraphicsStream.Context ctx = pipe.newContext()) {
+                subWidget.paint(ctx);
+                ctx.drawImage(AnyImage);
+            }
+        }
+    }
+
+    static class ExampleSubWidget extends AWidget {
+        @Override
+        public void paint(GraphicsStream pipe) {
+            GraphicsStream.Context ctx = pipe.newContext();
+            ctx.drawImage(AnyImage);
+            ctx.close();
         }
     }
 
