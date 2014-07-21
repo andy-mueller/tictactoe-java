369af30f8ca61d1b40ce63adb9cb63f60901393e
andy-mueller <andreas.b.mueller@freenet.de>
2014-07-22 00:49:45 +0200

    -intermediate

--- delivery/swing/src/main/java/com/crudetech/tictactoe/delivery/swing/grid/ScoreboardUI.java
+++ delivery/swing/src/main/java/com/crudetech/tictactoe/delivery/swing/grid/ScoreboardUI.java
@@ -1,8 +1,15 @@
 package com.crudetech.tictactoe.delivery.swing.grid;
 
+import com.crudetech.gui.widgets.Point;
+
 import javax.swing.*;
 import javax.swing.plaf.ComponentUI;
 import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 
 public class ScoreboardUI extends ComponentUI {
     @SuppressWarnings("unused")
@@ -10,35 +17,86 @@ public class ScoreboardUI extends ComponentUI {
         return new ScoreboardUI();
     }
 
+    static class ScoreboardUnitWidget {
+        static final double widthOverHeight = 0.85;
+        static final int baseHeight = 150;
+        Point location = Point.Origin;
+
+        int getWidth() {
+            return partOf(getHeight(), widthOverHeight);
+        }
+
+        private int partOf(int whole, double part) {
+            return (int) (whole * part);
+        }
+
+        int getHeight() {
+            return baseHeight;
+        }
+
+        Rectangle getNameRectangle() {
+            final double nameBoxHeightRatio = 0.3;
+            final double nameBoxWidthRatio = 0.85;
+            return null;
+        }
+
+        void paint(Graphics2D pipe) {
+            paintBackground(pipe);
+        }
+
+        private void paintBackground(Graphics2D pipe) {
+            pipe.setColor(Color.WHITE);
+            pipe.fill(new Rectangle(location.x, location.y, getWidth(), getHeight()));
+        }
+
+        private void paintDebugSeparators(Graphics pipe, int width, int height) {
+            final int horizontalSeparatorHeight = (int) (height * (1.0 / 3));
+
+            pipe.setColor(Color.RED);
+            pipe.drawLine(0, horizontalSeparatorHeight, width, horizontalSeparatorHeight);
+
+            int vertSeparator = (int) (width * (0.66));
+            pipe.drawLine(vertSeparator, horizontalSeparatorHeight, vertSeparator, 0);
+
+        }
+    }
+
     @Override
     public void paint(Graphics g, JComponent c) {
-        final int height = 150;
-        final int width = (int) (height * 0.85);
+        ScoreboardUnitWidget scoreboardUnit = new ScoreboardUnitWidget();
 
-        Graphics2D pipe = (Graphics2D) g;
-        Rectangle bounds = new Rectangle(0, 0, width, height);
-        pipe.setColor(Color.WHITE);
+        scoreboardUnit.paint((Graphics2D) g);
 
-        pipe.fill(bounds);
-
-        final int horizontalSeparatorHeight = (int) (height * (1.0 / 3));
-
-        pipe.setFont(new Font("Arial", Font.PLAIN, 20));
-        pipe.setColor(Color.RED);
-        final int textYPos = (horizontalSeparatorHeight - 20) / 2 + 20;
-        pipe.drawString("Marianne", 10, textYPos);
-
-
-        pipe.setFont(new Font("Arial", Font.PLAIN, 50));
-        pipe.setColor(Color.RED);
-        final int scoreYPos = 130;
-        pipe.drawString("12", 20, scoreYPos);
-
-
-        pipe.drawLine(0, horizontalSeparatorHeight, width, horizontalSeparatorHeight);
-
-        int vertSeparator = (int) (width * (0.66));
-        pipe.drawLine(vertSeparator, horizontalSeparatorHeight, vertSeparator, 0);
-
+//        paintDebugSeparators(g, width, height);
+//
+//        final int horizontalSeparatorHeight = (int) (height * (1.0 / 3));
+//
+//        pipe.setFont(new Font("Arial", Font.PLAIN, 20));
+//        pipe.setColor(Color.RED);
+//        final int textYPos = (horizontalSeparatorHeight - 20) / 2 + 20;
+//        String playerName = "Marianne";
+//        Graphics2D namePipe = (Graphics2D) pipe.create();
+//        namePipe.translate(10, textYPos);
+//        Rectangle2D boundary = measureStringBoundary(playerName, namePipe);
+//        namePipe.drawString(playerName, 0, 0);
+//        namePipe.setColor(Color.YELLOW);
+//        namePipe.draw(boundary);
+//        namePipe.dispose();
+//
+//        pipe.setFont(new Font("Arial", Font.PLAIN, 50));
+//        pipe.setColor(Color.RED);
+//        final int scoreYPos = 130;
+//        pipe.drawString("12", 20, scoreYPos);
     }
+
+    private Rectangle2D measureStringBoundary(String str, Graphics2D pipe) {
+        FontRenderContext fontRenderContext = pipe.getFontRenderContext();
+        Font currentFont = pipe.getFont();
+        return measureStringBoundary(str, currentFont, fontRenderContext);
+    }
+
+    private Rectangle2D measureStringBoundary(String str, Font currentFont, FontRenderContext fontRenderContext) {
+        return currentFont.getStringBounds(str, fontRenderContext);
+    }
+
 }
