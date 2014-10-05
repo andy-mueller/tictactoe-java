a518165673c02b4d3138f5f5dcfb76e084794800
andy-mueller <andreas.b.mueller@freenet.de>
2014-10-05 22:28:12 +0200

    first spike for a use case driven archiecture

--- game/src/test/java/com/crudetech/tictactoe/usecases/InitiateNewGameUseCaseTest.java
+++ game/src/test/java/com/crudetech/tictactoe/usecases/InitiateNewGameUseCaseTest.java
@@ -1,5 +1,129 @@
 package com.crudetech.tictactoe.usecases;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class InitiateNewGameUseCaseTest {
 
+    UseCaseFactory useCaseFactory = new UseCaseFactory();
+
+
+    @Test
+    public void givenTwoPlayers_newGameIsCreated() throws Exception {
+        UseCase.Presenter<InitiateGameUseCase.Response> presenter = presenterMockForResponse();
+        UseCase<InitiateGameUseCase.Response> uc = useCaseFactory.create("initiate-game");
+
+        UseCase.Request.Builder requestBuilder = uc.requestBuilder();
+        requestBuilder.withParameter("player1", 48);
+        requestBuilder.withParameter("player2", 84);
+
+        UseCase.Request request = requestBuilder.createRequest();
+
+        uc.execute(request, presenter);
+
+        InitiateGameUseCase.Response expected = new InitiateGameUseCase.Response();
+        expected.gameId = 42;
+        verify(presenter).display(expected);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <TResponse> UseCase.Presenter<TResponse> presenterMockForResponse() {
+        return (UseCase.Presenter<TResponse>) mock(UseCase.Presenter.class);
+    }
+
+    static class InitiateGameUseCase implements UseCase<InitiateGameUseCase.Response> {
+        public static class Request implements UseCase.Request {
+            public Object player1;
+            public Object player2;
+        }
+
+        public static class Response {
+            public Object gameId;
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+
+                Response response = (Response) o;
+
+                if (gameId != null ? !gameId.equals(response.gameId) : response.gameId != null) return false;
+
+                return true;
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hashCode(gameId);
+            }
+        }
+
+        @Override
+        public void execute(UseCase.Request request, UseCase.Presenter<Response> presenter) {
+            Response r = new Response();
+            r.gameId = 42;
+            presenter.display(r);
+        }
+
+        @Override
+        public UseCase.Request.Builder requestBuilder() {
+            return new MapRequestBuilder() {
+                @Override
+                public UseCase.Request createRequest() {
+                    Request r = new Request();
+                    r.player1 = valueFor("player1");
+                    r.player2 = valueFor("player2");
+                    return r;
+                }
+            };
+        }
+    }
+
+    interface UseCase<TResponse> {
+        void execute(Request request, Presenter<TResponse> presenter);
+
+        Request.Builder requestBuilder();
+
+        static interface Request {
+            public interface Builder {
+                void withParameter(String name, Object value);
+
+                Request createRequest();
+            }
+        }
+
+        public interface Presenter<TResponse> {
+            public void display(TResponse response);
+        }
+    }
+
+    static abstract class MapRequestBuilder implements UseCase.Request.Builder {
+        private final HashMap<String, Object> parameters = new HashMap<>();
+
+        @Override
+        public void withParameter(String name, Object value) {
+            parameters.put(name, value);
+        }
+
+        protected Object valueFor(String name) {
+            return Objects.requireNonNull(parameters.get(name));
+        }
+    }
+
+    public class UseCaseFactory {
+        @SuppressWarnings({"unchecked"})
+        public <TResponse> UseCase<TResponse> create(String useCaseId) {
+            switch (useCaseId) {
+                case "initiate-game":
+                    return (UseCase<TResponse>) new InitiateGameUseCase();
+                default:
+                    throw new IllegalArgumentException("Unknown use case");
+            }
+        }
+    }
 }
